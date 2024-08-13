a5468d4b39d9642d5f22e809298c7e468413dfa6
Santiago Monteiro <santiago.a.monteiro@uea.edu.br>
2024-08-13 12:41:31 -0400

    delete a answer

---
+++ src/domain/forum/application/use-cases/delete-answer.ts
@@ -0,0 +1,31 @@
+import { AnswersRepository } from '../repositories/answers-repository'
+
+type DeleteAnswerUseCaseRequest = {
+  authorId: string
+  answerId: string
+}
+
+type DeleteAnswerUseCaseResponse = {}
+
+export class DeleteAnswerUseCase {
+  constructor(private answerRepository: AnswersRepository) {}
+
+  async execute({
+    authorId,
+    answerId,
+  }: DeleteAnswerUseCaseRequest): Promise<DeleteAnswerUseCaseResponse> {
+    const answer = await this.answerRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found!')
+    }
+
+    if (authorId !== answer.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    await this.answerRepository.delete(answer)
+
+    return {}
+  }
+}
--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -2,7 +2,7 @@ import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 
-type AnswerProps = {
+export type AnswerProps = {
   authorId: UniqueEntityID
   questionId: UniqueEntityID
   content: string
